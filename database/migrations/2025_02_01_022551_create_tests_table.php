cc0ec6da8e235928aa3ca42e287c2df3328deee7
Adrian Holman <adriholman@outlook.com>
2025-01-31 23:39:05 -0300

    nuevas migraciones

---
+++ database/migrations/2025_02_01_022551_create_tests_table.php
@@ -0,0 +1,32 @@
+<?php
+// filepath: /C:/xampp/htdocs/protocoleva/database/migrations/2025_02_01_022533_create_tests_table.php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('values');
+            $table->text('value_options');
+            $table->foreignId('enterprise_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tests');
+    }
+};
